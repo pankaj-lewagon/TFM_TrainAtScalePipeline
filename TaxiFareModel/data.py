adf2d1227174c0b0139ec29a5d9184383b92e3e3
Pankaj Patel <pankajpython1706@gmail.com>
2021-08-25 12:04:01 +0100

    Commit mid way through day

---
+++ TaxiFareModel/data.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import joblib
+from  TaxiFareModel import params
+
+
+
+def get_data(nrows=10_000):
+    '''returns a DataFrame with nrows from google bucket bucket'''
+    df = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}", nrows=1000)
+    return df
+
+def clean_data(df, test=False):
+    df = df.dropna(how='any', axis='rows')
+    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
+    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
+    if "fare_amount" in list(df):
+        df = df[df.fare_amount.between(0, 4000)]
+    df = df[df.passenger_count < 8]
+    df = df[df.passenger_count >= 0]
+    df = df[df["pickup_latitude"].between(left=40, right=42)]
+    df = df[df["pickup_longitude"].between(left=-74.3, right=-72.9)]
+    df = df[df["dropoff_latitude"].between(left=40, right=42)]
+    df = df[df["dropoff_longitude"].between(left=-74, right=-72.9)]
+    return df
+
+
+def upload_model_to_gcp():
+
+    client = storage.Client()
+
+    bucket = client.bucket(BUCKET_NAME)
+
+    blob = bucket.blob(STORAGE_LOCATION)
+
+    blob.upload_from_filename('model.joblib')
+
+def save_model(reg):
+    """method that saves the model into a .joblib file and uploads it on Google Storage /models folder
+    HINTS : use joblib library and google-cloud-storage"""
+
+    # saving the trained model to disk is mandatory to then beeing able to upload it to storage
+    # Implement here
+    joblib.dump(reg, 'model.joblib')
+    print("saved model.joblib locally")
+
+    # Implement here
+    upload_model_to_gcp()
+    print(f"uploaded model.joblib to gcp cloud storage under \n => {STORAGE_LOCATION}")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    df = get_data()
---
+++ TaxiFareModel/utils.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def haversine_vectorized(df,
+                         start_lat="pickup_latitude",
+                         start_lon="pickup_longitude",
+                         end_lat="dropoff_latitude",
+                         end_lon="dropoff_longitude"):
+    """
+        Calculate the great circle distance between two points
+        on the earth (specified in decimal degrees).
+        Vectorized version of the haversine distance for pandas df
+        Computes distance in kms
+    """
+
+    lat_1_rad, lon_1_rad = np.radians(df[start_lat].astype(float)),\
+        np.radians(df[start_lon].astype(float))
+    lat_2_rad, lon_2_rad = np.radians(df[end_lat].astype(float)),\
+        np.radians(df[end_lon].astype(float))
+    dlon = lon_2_rad - lon_1_rad
+    dlat = lat_2_rad - lat_1_rad
+
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat_1_rad) * np.cos(lat_2_rad) *\
+        np.sin(dlon / 2.0) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return 6371 * c
+
+
+def compute_rmse(y_pred, y_true):
+    return np.sqrt(((y_pred - y_true) ** 2).mean())
+
+
+def df_optimized(df, verbose=True, **kwargs):
+    """
+    Reduces size of dataframe by downcasting numeircal columns
+    :param df: input dataframe
+    :param verbose: print size reduction if set to True
+    :param kwargs:
+    :return: df optimized
+    """
+    in_size = df.memory_usage(index=True).sum()
+    # Optimized size here
+    for type in ["float", "integer"]:
+        l_cols = list(df.select_dtypes(include=type))
+        for col in l_cols:
+            df[col] = pd.to_numeric(df[col], downcast=type)
+            if type == "float":
+                df[col] = pd.to_numeric(df[col], downcast="integer")
+    out_size = df.memory_usage(index=True).sum()
+    ratio = (1 - round(out_size / in_size, 2)) * 100
+    GB = out_size / 1000000000
+    if verbose:
+        print("optimized size by {} % | {} GB".format(ratio, GB))
+    return df
